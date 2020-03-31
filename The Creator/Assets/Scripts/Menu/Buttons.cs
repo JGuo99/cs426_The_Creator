f3132ab75dc697fbb1559039c694318d6b30c749
Anthony Desktop <aventu6@uic.edu>
2020-03-31 17:05:31 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ The Creator/Assets/Scripts/Menu/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour{
+
+    public void SpawnPlanet2() {
+        //Code to spawn planet one here
+    }
+
+    public void SpawnPlanet3() {
+        //Code to spawn planet one here
+    }
+
+    public void SpawnPlanet4() {
+        //Code to spawn planet one here
+    }
+
+    public void SpawnPlanet5() {
+        //Code to spawn planet one here
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
