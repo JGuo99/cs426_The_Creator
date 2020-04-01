9c42bbe4e7c573510189307d7578234b041df076
Anthony Desktop <aventu6@uic.edu>
2020-04-01 14:37:47 -0500

    deleted sean folder + add his orbits to main/replaced the standins

--- The Creator/Assets/Scripts/Menu/Buttons.cs
+++ The Creator/Assets/Scripts/Menu/Buttons.cs
@@ -10,6 +10,7 @@ public class Buttons : MonoBehaviour{
     [SerializeField] GameObject planet3;
     [SerializeField] GameObject planet4;
     [SerializeField] GameObject planet5;
+    [SerializeField] GameObject planet6;
 
     public void SpawnPlanet2() {
         planet2.gameObject.SetActive(true);
@@ -28,6 +29,11 @@ public class Buttons : MonoBehaviour{
 
     public void SpawnPlanet5() {
         planet5.gameObject.SetActive(true);
+        //Code to spawn planet one here
+    }
+
+    public void SpawnPlanet6() {
+        planet6.gameObject.SetActive(true);
         //Code to spawn planet one here
     }
 
