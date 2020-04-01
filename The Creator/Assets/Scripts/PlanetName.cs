520a968ffcfb2eb037371add0a68807dadb6e4f7
JGuo99 <jguo0310@gmail.com>
2020-04-01 13:58:19 -0500

    Added Planet Names and Output

---
+++ The Creator/Assets/Scripts/PlanetName.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetName : MonoBehaviour
+{
+    public Text nameTag;
+
+    void Update()
+    {
+        Vector3 nameTagPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        nameTag.transform.position = nameTagPos;
+    }
+}
