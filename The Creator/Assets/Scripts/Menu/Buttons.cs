ce968c707c793ffdc0105ff687eea1c3086c66c6
Anthony Desktop <aventu6@uic.edu>
2020-04-01 12:36:06 -0500

    Added functionality for spawning in planets 2-5 and trails

--- The Creator/Assets/Scripts/Menu/Buttons.cs
+++ The Creator/Assets/Scripts/Menu/Buttons.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class Buttons : MonoBehaviour{
 
     [SerializeField] GameObject planet2;
+    [SerializeField] GameObject planet3;
+    [SerializeField] GameObject planet4;
+    [SerializeField] GameObject planet5;
 
     public void SpawnPlanet2() {
         planet2.gameObject.SetActive(true);
@@ -14,14 +17,17 @@ public class Buttons : MonoBehaviour{
     }
 
     public void SpawnPlanet3() {
+        planet3.gameObject.SetActive(true);
         //Code to spawn planet one here
     }
 
     public void SpawnPlanet4() {
+        planet4.gameObject.SetActive(true);
         //Code to spawn planet one here
     }
 
     public void SpawnPlanet5() {
+        planet5.gameObject.SetActive(true);
         //Code to spawn planet one here
     }
 
