85fd4056cb1842f2c6def0b60d54ae1f877a58d1
Anthony Desktop <aventu6@uic.edu>
2020-04-07 02:01:31 -0500

    added UFO pathfinding

---
+++ The Creator/Assets/Scripts/AnthonyTestScripts/UFOPathfinding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOPathfinding : MonoBehaviour{
+    public Transform[] target;
+    public float speed;
+
+    private int currNode;
+
+    // Update is called once per frame
+    void Update(){
+
+        if(Vector3.Distance(transform.position, target[currNode].position) > .1f) { 
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[currNode].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        } else {
+            currNode = (++currNode) % target.Length;
+        }
+    }
+}
